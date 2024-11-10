3017b0249c845b8fa9fb55e680c6f34ef00948cd
Rishabh <cmpunk.mannu@gmail.com>
2024-11-10 21:09:15 +0530

    Added features using Flask Framework

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -17,7 +17,7 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object, evaluate_model
+from src.utils import save_object, evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
@@ -90,12 +90,16 @@ class ModelTrainer:
         }
       }
       
-      model_report:dict=evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test,y_test=y_test,models=models, param=params)
+      model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test,y_test=y_test,models=models, param=params)
       
       
       # Get the best model score and name
-      best_model_name=max(model_report,key=lambda x:model_report[x]['Test R2 Score'])
-      best_model_score=model_report[best_model_name]['Test R2 Score']
+      best_model_name = max(model_report, key=lambda x: model_report[x])
+      best_model_score = model_report[best_model_name]
+
+      
+      # best_model_name=max(model_report,key=lambda x:model_report[x]['Test R2 Score'])
+      # best_model_score=model_report[best_model_name]['Test R2 Score']
       
       if(best_model_score<0.6):
         raise CustomException("No best model found")
--- src/utils.py
+++ src/utils.py
@@ -1,68 +1,64 @@
 import os
 import sys
-import pandas as pd
-import numpy as np
-from src.exception import CustomException
-import dill
 
-from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+import numpy as np
+import pandas as pd
+import dill
+from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
-def save_object(file_path,obj):
-  try:
-    dir_path=os.path.dirname(file_path)
-    
-    os.makedirs(dir_path,exist_ok=True)
-    with open(file_path,'wb') as file:
-      dill.dump(obj,file)
-      
-  except Exception as e:
-    raise CustomException(e,sys)
-  
-  
-def evaluate_model(X_train,y_train,X_test,y_test,models,param):
-  try:
-    report={}
-    
-    for i in range(len(list(models))):
-      model=list(models.values())[i]
-      para = param[list(models.keys())[i]]
-      
-      gs=GridSearchCV(model,para,cv=3,n_jobs=-1)
-      gs.fit(X_train,y_train)
-      
-      model.set_params(**gs.best_params_)
-      model.fit(X_train,y_train)
-      
-      #model.fit(X_train,y_train) # Train the model
-      y_train_pred=model.predict(X_train)
-      y_test_pred=model.predict(X_test)
-      
-      # Calculate the r2 score
-      train_model_score=r2_score(y_train,y_train_pred)
-      test_model_score=r2_score(y_test,y_test_pred)
-      
-      # Calculate the Mean Squared Error
-      train_mse=mean_squared_error(y_train,y_train_pred)
-      test_mse=mean_squared_error(y_test,y_test_pred)
-      
-      # Calculate the Mean Absolute Error
-      train_mae=mean_absolute_error(y_train,y_train_pred)
-      test_mae=mean_absolute_error(y_test,y_test_pred)
-      
-      
-      
-      report[list(models.keys())[i]]={
-        "Train R2 Score":train_model_score,
-        "Test R2 Score":test_model_score,
-        "Train Mean Squared Error":train_mse,
-        "Test Mean Squared Error":test_mse,
-        "Train Mean Absolute Error":train_mae,
-        "Test Mean Absolute Error":test_mae
-      }
-      
-    return report
-  
-  except Exception as e:
-    raise CustomException(e,sys)
-    
+from src.exception import CustomException
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            # model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
